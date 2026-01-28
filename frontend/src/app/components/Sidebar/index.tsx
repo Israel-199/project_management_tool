188401e2e72318afbf0fd4e4e4c3f6de7c31a714
Israel-199 <israelassefa199@gmail.com>
2026-01-28 09:57:58 -0800

    adding the Sidebar component

--- frontend/src/app/components/Sidebar/index.tsx
+++ frontend/src/app/components/Sidebar/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 
 const Sidebar = () => {
+    const [showProjects, setShowProjects] = useState(true);
+    const [showPriority, setShowPriority] = useState(true);
+
+    const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+
   return (
-    <div>Sidebar</div>
+    <div className={sidebarClassNames}>Sidebar</div>
   )
 }
 
