e418a87ed34c654c5bae8e851211fcfff8450db5
Israel-199 <israelassefa199@gmail.com>
2026-01-28 09:13:20 -0800

    adding dashboardWrapper on the layout and make updated

--- frontend/src/app/dashboardWrapper.tsx
+++ frontend/src/app/dashboardWrapper.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-type Props = {}
 
-const DashboardWrapper = (props: Props) => {
+const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>DashboardWrapper</div>
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+            {children}
+        </main>
+    </div>
   )
 }
 
