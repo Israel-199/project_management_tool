89aec35fcab95817653dc2c1054eb966669fa685
Israel-199 <israelassefa199@gmail.com>
2026-01-28 11:14:01 -0800

    connecting dashboard to the store

--- frontend/src/app/dashboardWrapper.tsx
+++ frontend/src/app/dashboardWrapper.tsx
@@ -19,7 +19,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      
+        <DashboardLayout>{children}</DashboardLayout>
     </StoreProvider>
   )
 }
