6a27b9a79652ed02dc38e1354e0f777aaf627d0d
Israel-199 <israelassefa199@gmail.com>
2026-01-28 11:06:01 -0800

    formating the document

--- frontend/src/app/redux.tsx
+++ frontend/src/app/redux.tsx
@@ -54,7 +54,6 @@ const rootReducer = combineReducers({
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-/* REDUX STORE */
 export const makeStore = () => {
   return configureStore({
     reducer: persistedReducer,
@@ -67,27 +66,24 @@ export const makeStore = () => {
   });
 };
 
-/* REDUX TYPES */
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-/* PROVIDER */
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-    
- const storeRef = useRef<AppStore | null>(null);
+  const storeRef = useRef<AppStore | null>(null);
 
-if (!storeRef.current) {
-  storeRef.current = makeStore();
-  setupListeners(storeRef.current.dispatch);
-}
-const persistor = persistStore(storeRef.current);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+    setupListeners(storeRef.current.dispatch);
+  }
+  const persistor = persistStore(storeRef.current);
 
   return (
     <Provider store={storeRef.current}>
