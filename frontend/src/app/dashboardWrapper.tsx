a9b620f1c6b29bc69162fe498a0a879a971db505
Israel-199 <israelassefa199@gmail.com>
2026-01-28 09:20:25 -0800

    adding navbar component

--- frontend/src/app/dashboardWrapper.tsx
+++ frontend/src/app/dashboardWrapper.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import Navbar from "@/src/app/components/Navbar";
 
-
-const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
-            {children}
-        </main>
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+        <Navbar />
+        {children}
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
