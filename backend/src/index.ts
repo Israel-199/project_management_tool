09010d454206dad81ccabd8af0bdaff22ed138e7
Israel-199 <israelassefa199@gmail.com>
2026-01-29 04:51:56 -0800

    adding and import the project route to the index.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,28 @@
-import express from "express";
-import cors from "cors";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors()); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Backend server is running 🚀");
+  res.send("This is home route");
 });
 
-app.get("/api/health", (req, res) => {
-  res.json({ status: "ok", timestamp: new Date() });
-});
+app.use("/projects", projectRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on part ${port}`);
 });
