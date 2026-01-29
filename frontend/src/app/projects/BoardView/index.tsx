6d449d955fa270b1ae7bc15012c6377c700439df
Israel-199 <israelassefa199@gmail.com>
2026-01-29 04:01:02 -0800

    fix the import issue

--- frontend/src/app/projects/BoardView/index.tsx
+++ frontend/src/app/projects/BoardView/index.tsx
@@ -2,7 +2,7 @@ import { useGetTasksQuery, useUpdateTaskStatusMutation } from "@/state/api";
 import React from "react";
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { Task as TaskType } from "@/state/api";
+import { Task as TaskType } from "@/src/state/api";
 import { EllipsisVertical, MessageSquareMore, Plus } from "lucide-react";
 import { format } from "date-fns";
 import Image from "next/image";
