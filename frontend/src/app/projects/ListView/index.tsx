0e0ab0766ab585df5ca7ac3322900315bccb8c48
Israel-199 <israelassefa199@gmail.com>
2026-01-29 04:03:51 -0800

    fix the import issue

--- frontend/src/app/projects/ListView/index.tsx
+++ frontend/src/app/projects/ListView/index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/src/components/Header";
 import TaskCard from "@/components/TaskCard";
 import { Task, useGetTasksQuery } from "@/state/api";
 import React from "react";
