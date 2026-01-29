283520d05cd94c3b17f8eeff2bd2c32bb2dfcab9
Israel-199 <israelassefa199@gmail.com>
2026-01-29 04:52:53 -0800

    formate the document

--- backend/src/controllers/projectController.ts
+++ backend/src/controllers/projectController.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export const getProjects = async (
   req: Request,
-  res: Response
+  res: Response,
 ): Promise<void> => {
   try {
     const projects = await prisma.project.findMany();
@@ -19,7 +19,7 @@ export const getProjects = async (
 
 export const createProject = async (
   req: Request,
-  res: Response
+  res: Response,
 ): Promise<void> => {
   const { name, description, startDate, endDate } = req.body;
   try {
